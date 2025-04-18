ef445b1fd175fcf121ef02a7b10dc400dfb5140b
ahJohn831 <bladedestroyer959@gmail.com>
2025-04-18 22:13:12 +0800

    everything inside

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-bruh
+from flask import Flask, render_template, request, redirect
+
+app = Flask(name)
+todos = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=['POST'])
+def add():
+    todo = request.form.get('todo')
+    if todo:
+        todos.append(todo)
+    return redirect('/')
+
+if name == 'main':
+    app.run(debug=True)
