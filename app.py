46ac90402c4d937484d9267b6e6bd864012d0fb6
ahJohn831 <bladedestroyer959@gmail.com>
2025-04-23 13:28:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ todos = []
 
 @app.route("/")
 def index():
-    return render_template("index.html", todos=todos)
+    return render_template("index.html", todos=todoz)
 
 @app.route("/add", methods=["POST"])
 def add():
